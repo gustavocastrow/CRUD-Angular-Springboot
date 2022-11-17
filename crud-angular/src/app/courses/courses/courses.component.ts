d6dca7d513e2dc9c29dfb48f397e635560919eaf
gustavo castro <gustavocastrocs@gmail.com>
2022-11-17 09:31:20 -0300

    11/Tratamento de Erros

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,7 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
+import { catchError, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 
 import { Course } from '../model/course';
@@ -10,13 +13,30 @@ import { CoursesService } from './../services/courses.service';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ){
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string){
+    this.dialog.open(ErrorDialogComponent,{
+      data: errorMsg
+    });
+
   }
 
   ngOnInit(): void {
