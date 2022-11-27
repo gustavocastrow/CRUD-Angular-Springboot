350f123641c66df32b412ce621d6598e8d809911
gustavo castro <gustavocastrocs@gmail.com>
2022-11-27 16:36:14 -0300

    20/Salvando dados com Http post + tratamento de erros

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs/operators';
+import { first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,20 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) { } //injeção do httpClient
 
   list(){
-    return this.httpClient.get<Course[]>(this.API)
+    return this.httpClient.get<Course[]>(this.API) //buscando informações no servidor
     .pipe(
       first(),
       //delay(5000),
       tap(courses => console.log(courses))
     );
   }
+
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+
+  }
 }
+
+
+//A responsabilidade para onde os dados do form seram enviados
+//é do service
